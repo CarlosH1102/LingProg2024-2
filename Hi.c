7e683f87b58ae418b3c2484673b96e5701bcdb4a
CarlosH1102 <carlosoliveira.mt@hotmail.com>
2024-10-05 00:20:00 +0000

    Aaah

---
+++ Hi.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+        void main (){
+            int x=0; //Criando uma variavel inteira
+     printf("Valor de x: %d\n", x);
+     x++;  //Incrementando x em uma unidade
+     printf("Valor de x: %d\n ", x);
+     float y = 12.345678; //Criando uma variavel real
+     printf("Valor de y: %f\n", y);
+     // Imprimindo valor de y com duas casas
+     printf("Valor de y: %.2f\n", y);
+     char sexo = 'M';   //Váriavel que aceita um caractere (Sempre entre ' ' não " ")
+     printf("Sexo: %c\n", sexo); 
+     //Criando uma variavel String (Texto)
+     char nome[30] = "Joao";
+     printf("Nome: %s\n", nome);
+     //Imprimindo a primeira letra do nome
+     printf(" Primeira Letra: %c\n", nome[0]);
+     printf(" Hi Mundo");
+        }
